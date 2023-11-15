48a45757a65f009f8b878f31cc6a270b1dee3f06
NastyaOu <onufrienko869@gmail.com>
2023-11-15 14:37:48 +0300

    разделение логики игрока и противника

--- game.py
+++ game.py
@@ -4,14 +4,19 @@ import time
 from pygame.locals import *
 
 
-class Person: #описание класса игрового объекта
-    def __init__(self, x, y, speed, image):
+class Person:  # описание класса игрового объекта
+    def __init__(self, x, y, speed):
         self.x = x
         self.y = y
         self.speed = speed
-        self.image = pygame.image.load(image)
 
-    def move(self): #метод преследования игрока
+
+class Enemy(Person):
+    def __init__(self, x, y, speed):
+        super().__init__(x, y, speed)
+        self.image = pygame.image.load('enemy.png')
+
+    def move(self):  # метод преследования игрока
         if self.x < player.x:
             self.x += self.speed
         elif self.x > player.x:
@@ -21,18 +26,35 @@ class Person: #описание класса игрового объекта
         elif self.y > player.y:
             self.y -= self.speed
 
-        #проверка окончания игры
+        # проверка окончания игры
         global gameovercheck
-        #проверка столкновения
+        # проверка столкновения
         if self.x <= player.x + 64 and self.x >= player.x and self.y >= player.y and self.y <= player.y + 64:
             gameovercheck = True
             screen.blit(gameover, (0, 0))
-        if self.x+64 <= player.x + 64 and self.x+64 >= player.x and self.y+64 >= player.y and self.y+64 <= player.y + 64:
+        if self.x + 64 <= player.x + 64 and self.x + 64 >= player.x and self.y + 64 >= player.y and self.y + 64 <= player.y + 64:
             gameovercheck = True
             screen.blit(gameover, (0, 0))
 
 
-pygame.init()   #инициализация игры, отрисовка окна
+class Player(Person):
+    def __init__(self, x, y, speed):
+        super().__init__(x, y, speed)
+        self.image = pygame.image.load('player.png')
+
+    def move(self):  # управление персонажем
+        keys_pressed = pygame.key.get_pressed()
+        if (keys_pressed[K_UP] or keys_pressed[K_w]) and self.y > 0:
+            self.y -= self.speed
+        if (keys_pressed[K_DOWN] or keys_pressed[K_s]) and self.y < 656:
+            self.y += self.speed
+        if (keys_pressed[K_RIGHT] or keys_pressed[K_d]) and self.x < 1216:
+            self.x += self.speed
+        if (keys_pressed[K_LEFT] or keys_pressed[K_a]) and self.x > 0:
+            self.x -= self.speed
+
+
+pygame.init()  # инициализация игры, отрисовка окна
 screen = pygame.display.set_mode((1280, 720), 0, 32)
 pygame.display.set_caption("First Python Game!")
 back = pygame.image.load('back.png')
@@ -40,82 +62,71 @@ gameover = pygame.image.load('gameover.png')
 
 font = pygame.font.Font(None, 25)
 scorefont = pygame.font.Font(None, 60)
-score = 0   #количество очков
-gameovercheck = False #окончание игры
+score = 0  # количество очков
+gameovercheck = False  # окончание игры
 time0 = time.time()
 
 enemynum = 3
 lastadd = 0
 
-player = Person(100, 320, 5, 'player.png') #инициализация игрока
-allplayers = [player] #список игровых объектов
+player = Player(100, 320, 5)  # инициализация игрwaока
+allplayers = [player]  # список игровых объектов
 
 abouttext = font.render("GitHub: stepigor. Version 1.0.", True, (255, 255, 255))
 
 mainLoop = True
-while mainLoop: #игровой цикл
+while mainLoop:  # игровой цикл
 
     if gameovercheck == False:
-        
+
         if score == 3:
             try:
                 enemy1
                 enemy2
             except:
-                #создание первых противников
-                enemy1 = Person(1000, 250, 1, 'enemy.png')
-                enemy2 = Person(1000, 500, 1, 'enemy.png')
+                # создание первых противников
+                enemy1 = Enemy(1000, 250, 1)
+                enemy2 = Enemy(1000, 500, 1)
                 allplayers.append(enemy1)
                 allplayers.append(enemy2)
 
-        if score > 14 and score % 15 == 0 and score != lastadd: #добавление противников каждые 30 очков
+        if score > 14 and score % 15 == 0 and score != lastadd:  # добавление противников каждые 30 очков
             try:
                 globals()['enemy' + str(enemynum)]
             except KeyError:
-                globals()['enemy' + str(enemynum)] = Person(random.randint(0,1200),random.randint(0,650),random.randint(1,2),'enemy.png');
+                globals()['enemy' + str(enemynum)] = Enemy(random.randint(0, 1200), random.randint(0, 650),
+                                                           random.randint(1, 2))
                 allplayers.append(globals()['enemy' + str(enemynum)])
                 enemynum += 1
                 lastadd = score
 
-        #вычисление количества очков
+        # вычисление количества очков
         time1 = time.time()
         score = int(time1 - time0)
         scoretext = scorefont.render(str(score), True, (255, 255, 255))
 
         screen.blit(back, (0, 0))
-        #отрисовка игровых объектов
-        for i, item in enumerate(allplayers):
+        # отрисовка игровых объектов
+        for item in allplayers:
             screen.blit(item.image, (item.x, item.y))
-            if i != 0:
-                item.move()
-
-        #управление персонажем
-        keys_pressed = pygame.key.get_pressed()
-        if (keys_pressed[K_UP] or keys_pressed[K_w]) and player.y > 0:
-            player.y -= player.speed
-        if (keys_pressed[K_DOWN] or keys_pressed[K_s]) and player.y < 656:
-            player.y += player.speed
-        if (keys_pressed[K_RIGHT] or keys_pressed[K_d]) and player.x < 1216:
-            player.x += player.speed
-        if (keys_pressed[K_LEFT] or keys_pressed[K_a]) and player.x > 0:
-            player.x -= player.speed
+            item.move()
 
         screen.blit(abouttext, (3, 695))
         screen.blit(scoretext, (1200, 15))
 
     else:
-        #отрисовка проигрыша
+        # отрисовка проигрыша
         screen.blit(gameover, (0, 0))
         screen.blit(scoretext, (122, 400))
-        
+
         key_press = pygame.key.get_pressed()
-        #новая игра
+        # новая игра
         if key_press[K_RETURN]:
             time0 = time.time()
             time1 = time.time()
             score = 0
-            for i in range(1,enemynum):
-                del globals()['enemy'+str(i)]
+            for i in range(1, enemynum):
+                del globals()['enemy' + str(i)]
             lastadd = 0
             enemynum = 3
             allplayers = [player]
